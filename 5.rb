68f9ba732286278d709d92cd859470069325cc3a
Daniel <dacevedosp@gmail.com>
2019-06-15 11:46:23 -0400

    Finish exercise five 5.rb

--- 5.rb
+++ 5.rb
@@ -1,26 +1,37 @@
 module Herviboro
+
     Definir = 'Sólo me alimento de vegetales!'
-    def self.definir
+    def definir
         Definir
     end
+
     def dieta
         "Soy un Herviboro!"
     end
 end
 
 class Animal
+
     def saludar
         "Soy un animal!"
     end
+
 end
 
-class Conejo < Animal < Herviboro
+class Conejo < Animal 
+
+    include Herviboro
     def initialize(name)
         @name = name
     end
+
 end
 
 conejo = Conejo.new('Bugs Bunny')
 conejo.saludar
-conejo.dieta
-Herviboro.definir
+puts conejo.dieta
+#Herviboro.definir
+puts conejo.definir 
+
+#mixin juntas clases y modulos (ahorra codigo), solo en algunos casos es mas util.
+#la herencia trae los metodos al hijo...
