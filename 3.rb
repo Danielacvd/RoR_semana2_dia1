4fef61a46d9bdad34ce3dc1eb341c35e284760e3
Daniel <dacevedosp@gmail.com>
2019-06-15 11:29:34 -0400

    Finish four 4.rb and Start five 5.rb

--- 3.rb
+++ 3.rb
@@ -11,6 +11,32 @@ class Vehicle
 end
 
 class Car < Vehicle
+    @@instances = 0
+    def initialize
+        @@instances += 1
+    end
+    def engine_start
+        puts "El motor se encendio"
+    end
 
-    
+    def self.cars
+        @@instances
+    end
+
 end
+
+#creando y contando fuera de los metodos de la clase hija Car
+# cars = []
+# i = 0
+# 10.times do |x|
+#     cars << Car.new
+#     i += 1
+# end
+# puts "Entra #{i} veces"
+# puts cars
+#Car.new.engine_start
+
+
+# Creando los 10 autos en una sola linea
+10.times {Car.new.engine_start}
+puts "Se han fabricado #{Car.cars} Cars"
