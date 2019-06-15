49a2f37097265735051aa88f7ea20cdcbc663a55
Daniel <dacevedosp@gmail.com>
2019-06-14 20:53:08 -0400

    Start exercise 3 3.rb

---
+++ 3.rb
@@ -0,0 +1,16 @@
+class Vehicle
+    def initialize(model, year)
+        @model = model
+        @year = year
+        @start = false
+    end
+
+    def engine_start
+        @start = true
+    end
+end
+
+class Car < Vehicle
+
+    
+end
